24af77cd9447452a8942f1738bc2957704bb7c37
EdgardoSalinas <emsalinasl@gmail.com>
2024-07-02 10:53:07 -0500

    login + registro v1.0

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,51 @@
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// import cors from 'cors';
+// require('dotenv').config();
+// app.js
+
+
+require('dotenv').config();
+
 const express = require('express');
-const mongoose = require('mongoose');
+//C:\UTP_Semestre2024_I\DS9\Laboratorios\ProjFinal2024DS9v3\mongoose\db.js
+
+const cors = require('cors');
+
+
+const { connectToDatabase } = require('./mongoose/db');
 
 const app = express();
-const urlMongo = 'mongodb+srv://emsalinasl:utpds924@clusterdsix24.kzejgkh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDSIX24'
+app.use(cors());
+const userRoutes = require('./routes/rutaUsers');
 
-// Connect to MongoDB
-mongoose.connect(urlMongo, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Could not connect to MongoDB', err));
+app.use(express.json());
+app.use('/api/users', userRoutes);
 
-// Define routes and middleware here
+// Connect to the database
+connectToDatabase();
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// Rest of your Express app setup and routes go here
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// const app = express();
+// const urlMongo = 'mongodb+srv://emsalinasl:utpds924@clusterdsix24.kzejgkh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDSIX24'
+
+// // Connect to MongoDB
+// mongoose.connect(urlMongo, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(err => console.error('Could not connect to MongoDB', err));
+
+// // Define routes and middleware here
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
---
+++ backend/config.js
@@ -0,0 +1,8 @@
+// config.js
+
+module.exports = {
+    mongoURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/your_database_name'
+
+
+  };
+  
